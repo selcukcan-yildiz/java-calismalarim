077df872b0a3b4a72e600c9a3f5b0070ac390fc1
selcukcan-yildiz <selcukcanyildiz13@gmail.com>
2025-12-01 11:45:15 +0300

    Add files via upload
    html içinde java ilk alıştırmam

---
+++ script.js
@@ -0,0 +1,13 @@
+var username="selçuk can";
+var password="1234";
+if (username=="selçuk can"){
+if (password=="1234"){
+    console.log("kullanıcı giriş yaptı");
+}
+else{
+    console.log("şifre yanlış");
+}
+}
+else{
+     console.log("kullanıcı bulunamadı");
+}
