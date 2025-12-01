6cc00f1a717693ccb16bb9a47b78a3c89f2aeaa9
selcukcan-yildiz <selcukcanyildiz13@gmail.com>
2025-12-01 11:47:06 +0300

    Add files via upload
    javascript 2.alıştırmam ehliyet alabilme durumu incelendi.

---
+++ yeni.js
@@ -0,0 +1,8 @@
+var yas=10;
+var mezuniyet="üniversite";
+if (yas>=18 && (mezuniyet=="lise" || mezuniyet=="üniversite")){
+    console.log("kullanıcı sınava girebilir")
+}
+else{
+    console.log("kullanıcı sınava giriş yapamaz")
+}
